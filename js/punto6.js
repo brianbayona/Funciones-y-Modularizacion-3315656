5174e2873ed379366432ffdded5ebd48b9c76e37
joser <andrejoser39@gmail.com>
2026-03-26 10:11:32 -0500

    agregamos mensaje de inicio y arreglamos un mensaje para pedir informacion

--- js/punto6.js
+++ js/punto6.js
@@ -1,7 +1,9 @@
 let boletosVendidos = 0; //declaramos una variable para llevar el conteo de los boletos vendidos
 const LIMITE_BOLETOS = 15; //definimos una constante para el límite de boletos disponibles
+console.log("--- Inicio de la venta de boletos ---");//MENSAJE DE INICIO
 while (boletosVendidos < LIMITE_BOLETOS) {//iniciamos un ciclo que se ejecutará mientras el númerode boletos vendidos sea menor al límite
   let edadComprador = Math.floor(Math.random() * 30) + 10; //genero edad ente 10 y 39 años
   let esPeliculaAdultos = Math.random() < 0.5;//genera un valor booleano para adulto o no adulto
-  if (edadComprador < 18 && esPeliculaAdultos) {//si es menor no se le vende boleto para película
-    console.log(`No se vendió boleto a una persona de ${edadComprador} años para película de adultos.`);
+  console.log(`No se vendió boleto a una persona de ${edadComprador} años para película de adultos.`);//MENSAJE DE NO VENTA
+}
+if (edadComprador < 18 && esPeliculaAdultos) {//si es menor no se le vende boleto para película
