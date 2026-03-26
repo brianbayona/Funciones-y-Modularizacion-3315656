33630cb62de8bb2d6910108893d40119f7d03b0d
joser <andrejoser39@gmail.com>
2026-03-26 09:59:12 -0500

    declaracion de variables y creacion del archivo punto 6

---
+++ js/punto6.js
@@ -0,0 +1,2 @@
+let boletosVendidos = 0; //declaramos una variable para llevar el conteo de los boletos vendidos
+const LIMITE_BOLETOS = 15; //definimos una constante para el límite de boletos disponibles
