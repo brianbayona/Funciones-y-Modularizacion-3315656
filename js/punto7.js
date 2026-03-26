08b3f2fd504e0c99427413ea6a6b12a2757f6cfb
joser <andrejoser39@gmail.com>
2026-03-26 10:46:54 -0500

    mantenemos un contador de 1 - 100 con respectivio mensaje de salida

--- js/punto7.js
+++ js/punto7.js
@@ -1,3 +1,9 @@
 let suma = 0;//creamos una variable para almacenar la suma de los números
 
 console.log("--- Iniciando suma del 1 al 100 con interrupción ---");//MENSAJE DE INICIO
+for (let i = 1; i <= 100; i++) {//el contador empieza en 1 y va aunmentando hasta 100
+
+  if (i > 90 && i % 2 === 0) {// Verificamos si es mayor a 90 Y es par (el residuo de dividir entre 2 es 0)
+    console.log(`Ciclo interrumpido en el número: ${i}`);
+    break;// Si se cumple la condición, se imprime el mensaje y se interrumpe el ciclo con break
+  }
