46389a72ae3b95ec35601fd7edf4d8d1e4c79fa2
joser andres fuentes riano <andrejoser39@gmail.com>
2026-03-26 18:34:57 -0500

    terminando de comentar

--- js/punto6.js
+++ js/punto6.js
@@ -3,14 +3,14 @@ const LIMITE_BOLETOS = 15;
 /** Genera datos aleatorios del cliente y la película */
 function generarDatosVenta() {
     return {
-        edad: Math.floor(Math.random() * 30) + 10, // 10 a 39 años
-        esParaAdultos: Math.random() < 0.5         // true o false
+        edad: Math.floor(Math.random() * 30) + 10, // 10 a 39 años // Simulamos la edad del cliente
+        esParaAdultos: Math.random() < 0.5         // verificamos si es verdad o falso
     };
 }
 
 /** Valida si la venta es permitida según la edad */
 function esVentaValida(edad, esPeliculaAdultos) {
-    if (edad < 18 && esPeliculaAdultos) {
+    if (edad < 18 && esPeliculaAdultos) { //restringe la edad para ver peliculas de adultos
         return false;
     }
     return true;
@@ -18,14 +18,14 @@ function esVentaValida(edad, esPeliculaAdultos) {
 
 /** Procesa la lógica principal de la taquilla */
 function iniciarVenta() {
-    let boletosVendidos = 0;
+    let boletosVendidos = 0; // Contador de boletos vendidos 
     console.log("--- Inicio de la venta de boletos ---");
 
-    while (boletosVendidos < LIMITE_BOLETOS) {
+    while (boletosVendidos < LIMITE_BOLETOS) { //GENERA VENTAS HASTA QUE SE LLENE EL LIMITE DE BOLETOS
         const cliente = generarDatosVenta();
         
-        if (esVentaValida(cliente.edad, cliente.esParaAdultos)) {
-            boletosVendidos++;
+        if (esVentaValida(cliente.edad, cliente.esParaAdultos)) { //VALIDA SI LA VENTA ESTA PERMITIDA
+            boletosVendidos++; // INCREMENTA EL CONTADOR DE BOLETOS VENDIDOS
             console.log(` VENTA EXITOSA. Edad: ${cliente.edad} | Boletos: ${boletosVendidos}/${LIMITE_BOLETOS}`);
         } else {
             console.log(` VENTA DENEGADA. Menor de ${cliente.edad} años no puede ver película de adultos.`);
