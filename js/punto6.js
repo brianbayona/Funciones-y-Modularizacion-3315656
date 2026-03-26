2ce32a72065622bde1faa64a397c81ea7adb44bf
joser <andrejoser39@gmail.com>
2026-03-26 10:03:40 -0500

    agregamos la constante y tambien empezamos a simular datos

--- js/punto6.js
+++ js/punto6.js
@@ -1,2 +1,4 @@
 let boletosVendidos = 0; //declaramos una variable para llevar el conteo de los boletos vendidos
 const LIMITE_BOLETOS = 15; //definimos una constante para el límite de boletos disponibles
+while (boletosVendidos < LIMITE_BOLETOS) {//iniciamos un ciclo que se ejecutará mientras el númerode boletos vendidos sea menor al límite
+  let edadComprador = Math.floor(Math.random() * 30) + 10; //genero edad ente 10 y 39 años
