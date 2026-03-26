a667b39d078ab8f736ea24c7daa562c0aa702bb0
joser <andrejoser39@gmail.com>
2026-03-26 10:42:57 -0500

    agregamos comentarios en las lineas de codigo

--- js/punto7.js
+++ js/punto7.js
@@ -1,3 +1,3 @@
-let suma = 0;
+let suma = 0;//creamos una variable para almacenar la suma de los números
 
-console.log("--- Iniciando suma del 1 al 100 con interrupción ---");
+console.log("--- Iniciando suma del 1 al 100 con interrupción ---");//MENSAJE DE INICIO
