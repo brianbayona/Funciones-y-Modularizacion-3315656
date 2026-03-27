7555d438e1613686016e3242765d039c9cac5177
joser andres fuentes riano <andrejoser39@gmail.com>
2026-03-26 19:40:34 -0500

    creamos funciones y documentamos la verificacion

---
+++ js/punto8.js
@@ -0,0 +1,8 @@
+function esMayorDeEdad(edad) { // Función para verificar si una persona es mayor de edad//
+    return edad >= 18; // Retorna true si la edad es 18 o más, de lo contrario false//
+}
+
+function filtrarMayores(listaPersonas) { // Función para filtrar personas mayores de edad//
+    return listaPersonas.filter(persona => esMayorDeEdad(persona.edad)); // Utiliza el método filter para crear un nuevo array con las personas que cumplen la condición de ser mayores de edad//
+}
+
