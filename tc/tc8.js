0715f5d147f5a6ce5e199210ffcb649f2a486836
joser andres fuentes riano <andrejoser39@gmail.com>
2026-03-26 21:18:51 -0500

    usamos el constante para comparar numeros de usuario y el de la suerte

---
+++ tc/tc8.js
@@ -0,0 +1,7 @@
+const generarNumeroAleatorio = (min, max) => { // Genera un número aleatorio entre min y max (inclusive)//
+    return Math.floor(Math.random() * (max - min + 1)) + min; // Math.random() genera un número decimal entre 0 (inclusive) y 1 (exclusive)//
+    };
+    const verificarIntento = (secreto, usuario) => { // Compara el número secreto con el número ingresado por el usuario//
+    if (usuario === secreto) { // Si el número es correcto, devuelve "¡Correcto!"//
+        return "¡Felicidades! Has adivinado el número."; 
+    } else if (usuario < secreto) { // Si el número es menor, devuelve "Demasiado bajo"//
